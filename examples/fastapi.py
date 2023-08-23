08564180ebbe8f1547c8836737edb4e9a537045c
seria_ati <seria.ati@gmail.com>
2023-08-23 18:03:11 +0800

    upd: example

--- examples/fastapi.py
+++ examples/fastapi.py
@@ -1,4 +1,5 @@
 import os
+from typing import Dict
 
 import fastapi
 from dotenv import load_dotenv
@@ -11,6 +12,7 @@ app = fastapi.FastAPI()
 class App(fastapi.FastAPI):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
+
         load_dotenv()
         merchant_id = os.getenv("MERCHANT_ID")
         hash_key = os.getenv("HASH_KEY")
@@ -24,16 +26,28 @@ class App(fastapi.FastAPI):
             hash_iv=hash_iv,
             test=True,
         )
+        self.mac_id_map: Dict[str, str] = {}
 
     @app.get("/ecpay_redirect")
     async def ecpay_redirect(self, request: fastapi.Request) -> fastapi.Response:
         params = dict(request.query_params)
-        html = await self.ecpay.create_order(
+        mac_value, html = await self.ecpay.create_order(
             total_amount=params["TotalAmount"],
             trade_desc=params["TradeDesc"],
             item_name=params["ItemName"],
             return_url=params["ReturnURL"],
-            choose_payment=params["ChoosePayment"],
             custom_field_1=params["CustomField1"],
         )
+        self.mac_id_map[params["CustomField1"]] = mac_value
         return fastapi.Response(content=html, media_type="text/html")
+
+    @app.post("/ecpay_return")
+    async def ecpay_return(self, request: fastapi.Request) -> fastapi.Response:
+        params = dict(await request.form())
+        mac_value = params.pop("CheckMacValue")
+        if mac_value != self.mac_id_map[params["CustomField1"]]:  # type: ignore
+            return fastapi.Response(content="0|Error", status_code=400)
+        if params["RtnCode"] == "1":
+            # 付款成功
+            pass
+        return fastapi.Response(content="1|OK")
